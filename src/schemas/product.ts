8d70007e4e402fbc86baa89ffd999dbb8d147819
David Recordon <recordond@gmail.com>
2023-09-17 11:58:41 -0400

    Actually query users all the way through.

--- src/schemas/product.ts
+++ src/schemas/product.ts
@@ -9,7 +9,7 @@ export const product = () =>
       Slug: z.string(),
       "Government Accreditations": z.optional(z.array(z.string())),
       "Industry Accreditations": z.optional(z.array(z.string())),
-      Users: z.optional(z.array(z.string())),
+      "Used By": z.optional(z.array(z.string())),
     })
     .transform((obj) => ({
       id: obj["ID"],
@@ -20,5 +20,5 @@ export const product = () =>
         ...(obj["Government Accreditations"] ?? []),
         ...(obj["Industry Accreditations"] ?? []),
       ],
-      users: obj["Users"] ?? [],
+      users: obj["Used By"] ?? [],
     }));
--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -3,11 +3,13 @@ import { z } from "zod";
 export const user = () =>
   z
     .object({
+      ID: z.string(),
       Name: z.string(),
       Abbreviation: z.optional(z.string()),
       Products: z.optional(z.array(z.string())),
     })
     .transform((obj) => ({
+      id: obj["ID"],
       name: obj["Name"],
       abbreviation: obj["Abbreviation"] ?? "",
       products: obj["Products"] ?? [],
